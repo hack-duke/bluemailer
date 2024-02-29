a723c9d2d61ac791ded2be04061e925fd0804832
James Xu <james@jamesxu.ca>
2024-02-28 22:21:25 -0500

    basic

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bluemailer
+
+Email and APNS handler for Blueride. Will feed off of AMQP/RabbitMQ.
+
+## Considerations
+- Uses
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ mod api;
 mod mailer;
 use std::env;
 
+use lapin::{
+    message::DeliveryResult,
+    options::{BasicAckOptions, BasicConsumeOptions, BasicPublishOptions, QueueDeclareOptions},
+    types::FieldTable,
+    BasicProperties, Connection, ConnectionProperties,
+};
 use log;
 
 #[tokio::main]
@@ -13,4 +19,76 @@ async fn main() {
     let smtp_host = env::var("SMTP_HOST").unwrap();
     log::info!("Loaded SMTP configuration");
     let smtp_mailer = mailer::Mailer::create_mailer(smtp_username, smtp_password, smtp_host);
+
+    let uri = "amqp://localhost:5672";
+    let options = ConnectionProperties::default()
+        // Use tokio executor and reactor.
+        // At the moment the reactor is only available for unix.
+        .with_executor(tokio_executor_trait::Tokio::current())
+        .with_reactor(tokio_reactor_trait::Tokio);
+
+    let connection = Connection::connect(uri, options).await.unwrap();
+    let channel = connection.create_channel().await.unwrap();
+
+    let _queue = channel
+        .queue_declare(
+            "queue_test",
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .await
+        .unwrap();
+
+    let consumer = channel
+        .basic_consume(
+            "queue_test",
+            "tag_foo",
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .await
+        .unwrap();
+
+    consumer.set_delegate(move |delivery: DeliveryResult| async move {
+        let delivery = match delivery {
+            // Carries the delivery alongside its channel
+            Ok(Some(delivery)) => delivery,
+            // The consumer got canceled
+            Ok(None) => return,
+            // Carries the error and is always followed by Ok(None)
+            Err(error) => {
+                dbg!("Failed to consume queue message {}", error);
+                return;
+            }
+        };
+
+        // Do something with the delivery data (The message payload)
+
+        log::info!(
+            "logged data {:?}",
+            String::from_utf8(delivery.data.clone()).unwrap()
+        );
+
+        delivery
+            .ack(BasicAckOptions::default())
+            .await
+            .expect("Failed to ack send_webhook_event message");
+    });
+
+    channel
+        .basic_publish(
+            "",
+            "queue_test",
+            BasicPublishOptions::default(),
+            b"Hello world!",
+            BasicProperties::default(),
+        )
+        .await
+        .unwrap()
+        .await
+        .unwrap();
+
+    log::info!("awaiting next steps");
+
+    std::future::pending::<()>().await;
 }
